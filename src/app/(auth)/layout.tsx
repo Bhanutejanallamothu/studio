2e558c0ecbddacc01887979b97d6de17de7fd0c0
Bhanuteja <nallamothubhanuteja110@gmail.com>
2025-10-30 15:33:09 +0000

    doctype html html(lang="es", dir="ltr")     head         meta(name="view

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "@/components/app/logo";
+
 
 export default function AuthLayout({
   children,
@@ -6,13 +6,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-      <div className="w-full max-w-sm">
-        <div className="mb-8 flex justify-center">
-            <Logo />
-        </div>
+    <>
         {children}
-      </div>
-    </div>
+    </>
   );
 }
