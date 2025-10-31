4e3076b7708d477d35b63338701cb11313ff626b
Bhanuteja <nallamothubhanuteja110@gmail.com>
2025-10-31 02:12:56 +0000

    now make all cards in dashboard transprent

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card/60 text-card-foreground shadow-sm backdrop-blur-sm",
+      "rounded-lg border border-white/10 bg-white/5 text-card-foreground shadow-lg backdrop-blur-sm",
       className
     )}
     {...props}
